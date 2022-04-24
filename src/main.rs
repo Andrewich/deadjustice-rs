99e7b295c0882dffeb03345d4a7b65ae007d3ca5
Maxim Egorov <abd.andrew@gmail.com>
2022-04-24 18:34:42 +0500

    Added run fn.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+//#![cfg(windows)]
+// Let's put this so that it won't open the console
+//#![windows_subsystem = "windows"]
+
+//-----------------------------------------------------------------------------
+
+const PROP_FILE_NAME: &str = "deadjustice.prop";
+const BUILD: &str = env!("CARGO_PKG_VERSION_PRE");
+
+//-----------------------------------------------------------------------------
+
+fn run() {
+    if cfg!(debug_assertions) {
+        println!("Dead Justice DEBUG Build {BUILD}");
+    } else {
+        println!("Dead Justice Build Release {BUILD}");
+    }
+}
+
+//-----------------------------------------------------------------------------
+
 fn main() {
-    unimplemented!();
+    run();
 }
